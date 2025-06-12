4b0fc6b23fc141d67d6c91b2f875b05133c20b9d
eramit01 <ptamit7826@gmail.com>
2025-06-12 17:24:34 +0530

    removed unwanted comments

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,4 @@
-// import express from 'express';
-// const router=express.Router();
-// export const signup=router.post('/signup',(req,res)=>{
-    // res.status(200).json({message:" User Sign Up Karega Yaha par"});
-// });
-// export default router;
+
 import express from 'express';
 import { login, logout, signup } from '../controllers/auth.controller.js';
 const router = express.Router();
